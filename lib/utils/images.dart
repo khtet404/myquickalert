0ee6c5921ba0f7a9448c301febeecf567e9231b2
saiyi <saiyitaung@gmail.com>
2023-05-18 14:13:02 +0630

    initial commit

---
+++ lib/utils/images.dart
@@ -0,0 +1,20 @@
+/// Define Animation Type
+class AppAnim {
+  /// Loading Animation
+  static const loading = 'packages/myquickalert/assets/loading.gif';
+
+  /// Success Animation
+  static const success = 'packages/myquickalert/assets/success.gif';
+
+  /// Error Animation
+  static const error = 'packages/myquickalert/assets/error.gif';
+
+  /// Warning Animation
+  static const warning = 'packages/myquickalert/assets/warning.gif';
+
+  /// Info Animation
+  static const info = 'packages/myquickalert/assets/info.gif';
+
+  /// Confirm Animation
+  static const confirm = 'packages/myquickalert/assets/confirm.gif';
+}
